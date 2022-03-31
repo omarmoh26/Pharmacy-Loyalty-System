8737670e66bfafb3a84e72e56314f601c691e8f9
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-03-31 20:58:12 +0200

    databasename

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'miublog');
+define('DB_NAME', 'pharmacy_loyalty_system');
 
 // App Root
 //echo dirname(dirname(__FILE__));
