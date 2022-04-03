f7e982791aafbee83cec2ed675d51c90121c5625
Ahmed Bakr <ahmed1902082@miuegypt.edu.eg>
2022-04-03 21:58:18 +0200

    logout from all pages

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -15,7 +15,7 @@
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <?php if (isset($_SESSION['user_id'])) : ?>
-              <li><a class="dropdown-item" href="users/logout">Logout</a></li>
+              <li><a class="dropdown-item" href="../users/logout">Logout</a></li>
             <?php else : ?>
               <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/login'; ?>">Login</a></li>
               <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/register'; ?>">Sign Up</a></li>
