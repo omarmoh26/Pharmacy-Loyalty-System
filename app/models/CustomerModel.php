ef938079ba9b2629b6d0f8025153befe253dad3e
Ahmed Bakr <ahmed1902082@miuegypt.edu.eg>
2022-04-03 21:43:32 +0200

    customer model

---
+++ app/models/CustomerModel.php
@@ -0,0 +1,48 @@
+<?php
+class CustomerModel extends model
+{
+    protected $phone_number;
+    protected $phone_numberErr;
+
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->phone_number = '';
+        $this->phone_numberErr = '';
+    }
+    
+    //Phone Number
+    public function getPhone_number()
+    {
+        return $this->phone_number;
+    }
+    public function setPhone_number($phone_number)
+    {
+        $this->phone_number = $phone_number;
+    }
+
+    //Phone number err
+    public function getPhone_numberErr()
+    {
+        return $this->phone_numberErr;
+    }
+    public function setPhone_numberErr($phone_numberErr)
+    {
+        $this->phone_numberErr = $phone_numberErr;
+    }
+
+    public function findUserByPhone_number($phone_number)
+    {
+        $this->dbh->query('select * from customer where phone_number= :phonenum');
+        $this->dbh->bind(':phonenum', $phone_number);
+
+        $userRecord = $this->dbh->single();
+        return $this->dbh->rowCount();
+    }
+
+    public function Phone_numberExist($phone_number)
+    {
+        return $this->findUserByID($phone_number) > 0;
+    }
+}
