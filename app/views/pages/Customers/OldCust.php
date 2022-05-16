7d7044af62ac9dee051af00eb09b0a7b0393e05f
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2022-05-16 23:44:50 +0200

    OldCustomer View

--- app/views/pages/Customers/OldCust.php
+++ app/views/pages/Customers/OldCust.php
@@ -1,79 +1,85 @@
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+<head><link rel="stylesheet" href="<?php echo URLROOT; ?>css/Viewusers.css"></head>
+<a class="back" href="<?php echo URLROOT . 'pages/User'; ?>">Back</a>
+<a class="leftbar" href="<?php echo URLROOT . 'users/Addemployee'; ?>">add employees</a>
 <?php
 class OldCust extends view
 {
+  
   public function output()
   {
-    $title = $this->model->title;
 
     require APPROOT . '/views/inc/header.php';
-    flash('register_success');
-    $this->printForm();
-    require APPROOT . '/views/inc/footer.php';
-  }
-
-  private function printForm()
-  {
-    $action = URLROOT . 'pages/Order';
-    ?>
-    <a class="back" href="<?php echo URLROOT . 'pages/User'; ?>">Back</a>
-    <?php
-    $text = <<<EOT
-    <div class="container">
-		<div class="row main">
-			<div class="panel-heading">
-				<div class="panel-title text-center">
-          <h1>Old Customer</h1>
-				</div>
-			</div> 
-			<div class="main-login main-center">
-    <form action="$action" method="post">
-EOT;
-
-    echo $text;
-    $this->printPhone_number();
-
-    $text = <<<EOT
-    <div class="form-group">
-    <div class="cols-sm-10">
-          <input type="submit" value="Login" class="form-control btn btn-lg btn-primary btn-block" >
-          </div>
-          <div class="message" id="message_name">
-          </div>
-        </div>
-      
-    </form>
     
-EOT;
-    echo $text;
-  }
-
-  private function printPhone_number()
-  {
-    $val = $this->model->getPhone_number();
-    $err = $this->model->getPhone_numberErr();
-    $valid = (!empty($err) ? 'is-invalid' : '');
-
-    $this->printInput('text', 'phone_number', $val, $err, $valid);
-  }
-  
-  private function printInput($type, $fieldName, $val, $err, $valid)
-  {
-    $label = str_replace("_", " ", $fieldName);
-    $label = ucwords($label);
+    $action = URLROOT . 'pages/Order';
     $text = <<<EOT
-    <div class="form-group">
-    <div class="cols-sm-10">
-      <div class="input-group">
+    <form action="$action" method="post">
+         
+    EOT;
+    ?>
 
-      <label for="$fieldName"> $label:</label>
-      <input type="$type" name="$fieldName" class="form-control form-control-lg $valid" id="$fieldName" value="$val" required="">
-      <span class="invalid-feedback">$err</span>
-    </div>
-      <div class="message" id="message_mail">
-      </div>
-    </div>
-  </div>
-EOT;
-    echo $text;
-  }
+<div class="containn">
+        <div class="container bootstrap snippets bootdey">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="main-box no-header clearfix">
+                        <div class="main-box-body clearfix">
+                            <div class="table-responsive">
+                                        <table class="table user-list">
+                                        <thead>
+                                            <tr>
+                                                <th><span>ID</span></th>
+                                                <th><span>Name</span></th>
+                                                <th><span>Phone Number</span></th>
+                                                <th><span>Address</span></th>
+                                                <th>&nbsp;</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php 
+                                        $result=$this->model->getAllCustomers();
+
+                                        while($row=mysqli_fetch_array($result)){
+                                            ?>
+                                                <tr>
+                                                <td><?php echo $row['id'] ?></td>
+                                                <td><?php echo $row['name'] ?></td>
+                                                <td><?php echo $row['phone_number'] ?></td>
+                                                <td><?php echo $row['address'] ?></td>
+                                                
+                                                <td style="width: 20%;">
+                                                    <a class="table-link text-info" href="<?php echo URLROOT . 'pages/Editemployee'; ?>?id=<?php echo $row['id'] ?>">
+                                                        <span class="fa-stack">
+                                                            <i class="fa fa-square fa-stack-2x"></i>
+                                                            <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                                        </span>
+                                                    </a>
+                                                    <a class="table-link danger" href="<?php echo URLROOT . 'pages/Deleteemployee'; ?>?id=<?php echo $row['id'] ?>">
+                                                        <span class="fa-stack">
+                                                            <i class="fa fa-square fa-stack-2x"></i>
+                                                            <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                                        </span>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                        
+                                        
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+      <?php
+      <<<EOT
+      </form>
+    
+      EOT;
+      echo $text;
+      require APPROOT . '/views/inc/footer.php';
+    }
 }
