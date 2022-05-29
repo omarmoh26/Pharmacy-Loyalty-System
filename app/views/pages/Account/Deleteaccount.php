7fa51c65c5a4c1a53c70e451c68931129ee6be42
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-05-29 23:41:03 +0200

    Delete account+ frontend/validation js

--- app/views/pages/Account/Deleteaccount.php
+++ app/views/pages/Account/Deleteaccount.php
@@ -1,35 +1,53 @@
-<head><link rel="stylesheet" href="<?php echo URLROOT; ?>css/Deleteemployee.css"></head>
+<head>
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/Deleteemployee.css">
+</head>
 
 <?php
 class Deleteaccount extends View
 {
-  
-    public function output()
-    {
-  
-      require APPROOT . '/views/inc/header.php';
+
+  public function output()
+  {
+
+    require APPROOT . '/views/inc/header.php';
+
+    $action = URLROOT . 'users/logout';
+
+    $text = <<<EOT
+    
+      <form action="$action" onsubmit="return(validate());" name="myForm" method="post">
       
-      $action = URLROOT . 'users/logout';
-      $text = <<<EOT
-      <form action="$action" method="post">
-      
-      <input type="submit" name="submit" value="Delete">
+      <input type="submit" name="submit" value="Delete";>
       
       EOT;
-      ?>
-        <div class="w">
-            <div class="midtext2">
-                <p1>Are you sure you want to delete this Account?</p1>
-            </div>
-        </div>
-        <a class="cancel" href="<?php echo URLROOT . 'pages/Admin'; ?>">Cancel</a>
-      <?php
-      <<<EOT
+?>
+    <p id="demo"></p>
+    <div class="w">
+      <div class="midtext2">
+        <p1>Are you sure you want to delete this Account?</p1>
+      </div>
+    </div>
+    <a class="cancel" href="<?php echo URLROOT . 'pages/Admin'; ?>">Cancel</a>
+<?php
+    <<<EOT
       </form>
     
       EOT;
-      echo $text;
-      require APPROOT . '/views/inc/footer.php';
-    }
+    echo $text;
+    require APPROOT . '/views/inc/footer.php';
+  }
 }
-
+?>
+<script>
+function validate() {
+  let text = "To confirm press [ok]";
+  if (confirm(text) == true) {
+    text = "Account Deleted";
+    
+  } 
+  else {
+    return false;
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+</script>
