b9ce4153142d93f13b10b844362afb2548f14f13
abdelrahman hesham <abdelrahman1902703@miuegypt.edu.eg>
2022-05-29 23:45:24 +0200

    edit name/validation

--- app/views/pages/Account/Editname.php
+++ app/views/pages/Account/Editname.php
@@ -16,16 +16,18 @@ class Editname extends view
     {
         $action = URLROOT . 'pages/Editname';
         $text = <<<EOT
-
+        
     <div class="container">
 		<div class="row main">
 			<div class="panel-heading">
-				<div class="panel-title text-center">
+				<div class="texttt">
     <h1>Edit Name</h1>
 				</div>
 			</div> 
 			<div class="main-login main-center">
-    <form action="$action"class="form-horizontal" method="post">
+    <form action="$action"name="myForm" class="form-horizontal" method="post" onsubmit="return(validate());">
+    <span id="demo"></span>
+        <br>
 EOT;
     echo $text;
     $this->printAdminName();
@@ -85,7 +87,7 @@ EOT;
                     <div class="input-group">
                   
             <label for="$fieldName"> $label:</label>
-            <input type="$type" name="$fieldName" class="form-control form-control-lg $valid" id="$fieldName" value="$val" required="">
+            <input type="$type" name="$fieldName" class="form-control form-control-lg $valid" id="$fieldName" value="$val" required="" onkeyup="return(validate());">
             <span class="invalid-feedback">$err</span>
             </div>
             <div class="message" id="message_mail">
@@ -113,3 +115,41 @@ EOT;
     }
   }
 }
+?>
+<script type="text/javascript">
+  // Form validation code will come here.
+  var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
+  var upp = new RegExp(
+    "^(?=.*[A-Z]).+$"
+  );
+  var loww = new RegExp(
+    "^(?=.*[a-z]).+$"
+  );
+  var digitt = new RegExp(
+    "^(?=.*\\d).+$"
+  );
+  function validate() {
+    
+    //////NAME
+    if (document.myForm.name.value.length == "") {
+      document.getElementById("demo").innerHTML ="Please provide a name" 
+      return false;
+    } 
+    else if (!(/^[a-zA-Z]+$/.test(document.myForm.name.value))) {
+      document.getElementById("demo").innerHTML ="name must contain letters only"  
+      return false;
+    }
+    //////USERNAME
+    if (document.myForm.username.value == "") {
+      document.getElementById("demo").innerHTML ="Please provide your username" 
+      return false;
+    } 
+    else{
+    document.getElementById("demo").innerHTML = "";
+    return (true);
+    
+    }
+  }
+  
+  //-->
+</script>
