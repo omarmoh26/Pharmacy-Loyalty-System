1efdf379eafb97dc31853eed04a261e066a12d01
Ahmed Bakr <ahmed1902082@miuegypt.edu.eg>
2022-05-29 23:31:27 +0200

    Delete employees/validation js

--- app/views/pages/Viewemployees.php
+++ app/views/pages/Viewemployees.php
@@ -1,82 +1,102 @@
 <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
-<head><link rel="stylesheet" href="<?php echo URLROOT; ?>css/Viewusers.css"></head>
+
+<head>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>css/Viewusers.css">
+</head>
 <?php
 class Viewemployees extends View
 {
-  
+
     public function output()
     {
-  
-      require APPROOT . '/views/inc/header.php';
-      
-      $action = URLROOT . 'pages/Viewemployees';
-      $text = <<<EOT
+
+        require APPROOT . '/views/inc/header.php';
+
+        $action = URLROOT . 'pages/Viewemployees';
+        $text = <<<EOT
       <form action="$action" method="post">
            
       EOT;
-      ?>
+?>
+        <span id="demo"></span>
 
-<div class="containn">
-        <div class="container bootstrap snippets bootdey">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="main-box no-header clearfix">
-                        <div class="main-box-body clearfix">
-                            <div class="table-responsive">
-                                        <table class="table user-list">
+        <div class="containn">
+            <div class="container bootstrap snippets bootdey">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="main-box no-header clearfix">
+                            <div class="main-box-body clearfix">
+                                <div class="table-responsive">
+                                    <table class="table user-list">
                                         <thead>
                                             <tr>
                                                 <th><span>ID</span></th>
                                                 <th><span>Name</span></th>
                                                 <th><span>Username</span></th>
-                                                <th><span>Type</span></th>                                                <th>&nbsp;</th>
+                                                <th><span>Type</span></th>
+                                                <th>&nbsp;</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                        <?php 
-                                        $result=$this->model->getAllEmployees();
+                                            <?php
+                                            $result = $this->model->getAllEmployees();
 
-                                        while($row=mysqli_fetch_array($result)){
+                                            while ($row = mysqli_fetch_array($result)) {
                                             ?>
                                                 <tr>
-                                                <td><?php echo $row['id'] ?></td>
-                                                <td><?php echo $row['name'] ?></td>
-                                                <td><?php echo $row['username'] ?></td>
-                                                <td><?php echo $row['type'] ?></td>
+                                                    <td><?php echo $row['id'] ?></td>
+                                                    <td><?php echo $row['name'] ?></td>
+                                                    <td><?php echo $row['username'] ?></td>
+                                                    <td><?php echo $row['type'] ?></td>
 
-                                                <td style="width: 20%;">
-                                                    <a class="table-link text-info" href="<?php echo URLROOT . 'users/Editemployee'; ?>?id=<?php echo $row['id'] ?>">
-                                                        <span class="fa-stack">
-                                                            <i class="fa fa-square fa-stack-2x"></i>
-                                                            <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-                                                        </span>
-                                                    </a>
-                                                <a class="table-link danger" href="<?php echo URLROOT . 'users/Deleteemployee'; ?>?id=<?php echo $row['id'] ?>">
-                                                    <span class="fa-stack">
-                                                        <i class="fa fa-square fa-stack-2x"></i>
-                                                        <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
-                                                    </span>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                        <?php } ?>
-                                    </tbody>
-                               
-                                    
-                                </table>
+                                                    <td style="width: 20%;">
+                                                        <a class="table-link text-info" href="<?php echo URLROOT . 'users/Editemployee'; ?>?id=<?php echo $row['id'] ?>">
+                                                            <span class="fa-stack">
+                                                                <i class="fa fa-square fa-stack-2x"></i>
+                                                                <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+                                                            </span>
+                                                        </a>
+                                                        <button onclick="return(validate())">
+                                                            <a class="table-link danger" href="<?php echo URLROOT . 'users/Deleteemployee'; ?>?id=<?php echo $row['id'] ?>">
+                                                                <span class="fa-stack">
+                                                                    <i class="fa fa-square fa-stack-2x"></i>
+                                                                    <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>
+                                                                </span>
+                                                        </button>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+
+
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>    
-      <?php
-      <<<EOT
+<?php
+        <<<EOT
       </form>
     
       EOT;
-      echo $text;
-      require APPROOT . '/views/inc/footer.php';
+        echo $text;
+        require APPROOT . '/views/inc/footer.php';
     }
 }
+?>
+<script>
+    function validate() {
+        let text = "To confirm press [ok]";
+        if (confirm(text) == true) {
+            text = "Employee Deleted";
+
+        } else {
+            return false;
+        }
+        document.getElementById("demo").innerHTML = text;
+    }
+</script>
