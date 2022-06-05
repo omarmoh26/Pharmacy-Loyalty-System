a731b2617434d73a33725c6db316ed6feff9947b
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-06-05 03:44:38 +0200

    nav bar /edit

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['user_id'])) {
       <!-- Sidebar  -->
       <nav id="sidebar">
         <div class="sidebar-header">
-          <h3">Saraya Care</h3>
+          <h3" style="font-size:30px; font-weight: bold;">Saraya Care</h3>
         </div>
 
         <ul class="list-unstyled components">
